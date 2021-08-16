a7c41a184ea0af9c4f2aeecb0b158b9eed4bb59f
VeremchukStanislav <verenchuk.stanislav@gmail.com>
2021-08-16 21:07:58 +0300

    Updates

---
+++ static/js/NotFound.339bc05d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[5],{511:function(t,e,n){"use strict";n.r(e);var s=n(1);e.default=function(){return Object(s.jsx)("div",{className:"Content",children:Object(s.jsx)("h1",{className:"Content-title",children:"404 - Page not found"})})}}}]);
+//# sourceMappingURL=NotFound.339bc05d.chunk.js.map
